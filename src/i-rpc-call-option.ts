80f1b461414426f845b8b87b471217cad68651f9
dengwenbin <dengwenbin@fengling.cn>
2023-03-08 16:48:13 +0800

    feat: 删除method

--- src/i-rpc-call-option.ts
+++ src/i-rpc-call-option.ts
@@ -1,7 +1,4 @@
-import { HttpMethod } from './http-method';
-
 export interface IRpcCallOption {
-    method: HttpMethod;
     route: string;
     body?: { [key: string]: any; };
     header?: { [key: string]: string; };
